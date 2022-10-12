9952de492bbb38863d8a47305f57dbf16ed45aa3
Elisa Bobadilla Martinez <54122933+ElisaBoMa@users.noreply.github.com>
2022-10-12 00:40:05 -0500

    -Avance1

---
+++ Assets/scripts/Asteroid.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+
+{
+	Transform myT;
+	Vector3 randomRotation;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        myT=Transform;
+
+    }
+    void start(){
+    	Vector3 scale=3;
+    	sacale.x=Random.Range(.8f,1.2f);
+    	myT.localScale=scale;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/playerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+	[SerialzeField]float movementSpeed = 100f;
+	[SerialzeField]float turnSpeed = 60f;
+    
+    transform myT;
+    void Awake(){
+    	myT = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Turn();
+        Thrust();
+    }
+     void Turn(){
+
+     }
+
+    void Thrust(){
+    	if(Input.GetAxis("Vertical")>0)
+    		myT.position += myT.forward = movementSpeed *Time.deltaTime * Input.GetAxis("Vertical");
+    }
+}
